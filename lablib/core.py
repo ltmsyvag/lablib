1ddb375844e09cbc9c6dc252594e9f2bbb3cf7ca
ltmsyvag <ltmsyvag@sharklasers.com>
2025-02-11 14:44:47 +0800

    Update core.py
    add peak2binary func

--- lablib/core.py
+++ lablib/core.py
@@ -1,2 +1,11 @@
-def greet(name):
-    return f"Hello, {name}!"
+def peaks2binary(nWinPnts, analogData, height=1):
+    """
+    convert analog data series (peaks) into binary data (1001020...), which is returned.
+    bin size is nWinPnts, the tailing points less than nWinPnts is thrown away
+    """
+    idPeaks, _ = find_peaks(analogData, height=height)
+    peakPredicates = np.array([False]*len(analogData))
+    for id in idPeaks: peakPredicates[id] = True
+    binary = peakPredicates[:-(len(analogData)%nWinPnts)].reshape((-1, nWinPnts))
+    binary = binary.sum(axis=1)
+    return binary
