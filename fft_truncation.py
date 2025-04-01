be1500ee9faf689808b744b685755d906b2f1f80
haiteng <htwu@phy.ecnu.edu.cn>
2025-04-01 12:12:17 +0800

    test

--- fft_truncation.py
+++ fft_truncation.py
@@ -1,15 +1,17 @@
 #%%
 import numpy as np
 import matplotlib.pyplot as plt
-from lablib102 import fdata_keep_n_lowfreq_pnts
+from lablib102.core import _fdata_keep_n_lowfreq_pnts
+# by the magic of a __init__.py layer for direct core.py importation, what won't work is: from lablib102 import _fdata_keep_n_lowfreq_pnts
 from scipy.fft import fft, ifft
 
 data = np.random.randn(8)
 fdata = fft(data)
 ifdata = ifft(fdata)
-fdata_filtered = fdata_keep_n_lowfreq_pnts(fdata, 3)
+fdata_filtered = _fdata_keep_n_lowfreq_pnts(fdata, 3)
 ifdata_filtered = ifft(fdata_filtered)
 
 fig, ax = plt.subplots()
 for id, e in enumerate([data, np.abs(fdata), ifdata.real, ifdata_filtered.real]):
     ax.plot(e, ls = ":" if id ==3 else None, color = "k" if id ==0 else None)
+# %%
